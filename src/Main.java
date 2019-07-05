eabc5cb1c20d21bfc8d25b6d7af630649785f704
Komlovski <evgeny.komlovsky@gmx.de>
2019-07-05 18:08:59 +0200

    Gui erstellt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import javax.swing.*;
+
 public class Main {
+    //Variablen GUI
+
+    JFrame fr = null;
+    JMenuBar jmb = null;
+    JMenu jm = null;
+    JPanel contantPane = null;
+
+//Konstruktor Gui
+    public Main(){
+        this.fr = new JFrame("Coutinius Integration");
+        this.fr.setSize(666,900);
+
+        this.jmb = new JMenuBar();
+        this.fr.setJMenuBar(jmb);
+        this.jm = new JMenu("File");
+        this.jmb.add(jm);
+
+
+        this.contantPane = new JPanel();
+        fr.setContentPane(contantPane);
+
+
+        fr.setVisible(true);
+    }
+
+
+
+
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Main mn = new Main();
+
     }
 }
