a769f1599192fa07fa54e79927136fe63e96b793
Hauch IT <Daniel.Hauch@ai.hs-fulda.de>
2019-07-05 18:29:08 +0200

    geburtstag sauber

--- src/Geburtstag.java
+++ src/Geburtstag.java
@@ -1,3 +1,19 @@
 public class Geburtstag {
 
+
+    public Geburtstag(){
+
+        Main mn1 = new Main();
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Geburtstag gb = new Geburtstag();
+
+
+    }
 }
+
