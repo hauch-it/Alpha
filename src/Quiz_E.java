46cc8d8081dcf77941d97b7fb46c60bf1c176d82
Komlovski <evgeny.komlovsky@gmx.de>
2019-07-06 14:51:05 +0200

    Beginn mit Quiz Spiel. //erste Giu elemente

--- src/Quiz_E.java
+++ src/Quiz_E.java
@@ -1,5 +1,7 @@
+import java.awt.JobAttributes;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 import javax.swing.*;
 public class Quiz_E {
 
@@ -9,12 +11,18 @@ public class Quiz_E {
   JLabel label1 = null;
   JLabel label2 = null;
   JLabel label3 = null;
+  JLabel label4 = null;
+  JLabel label5 = null;
+  JLabel label6 = null;
+  JLabel label7 = null;
   JTextField j1 = null;
   JTextField j2 = null;
   JTextField j3 = null;
+  JTextField j4 = null;
+
   JMenuItem menuItem1 = null;
   JMenuItem menuItem2 = null;
-
+  JOptionPane opane = null;
   JMenuBar bar =null;
   JMenu mF= null;
   JMenu mH = null;
@@ -24,7 +32,7 @@ public class Quiz_E {
   public Quiz_E() {
 
     this.fr = new JFrame("GUI");
-    this.fr.setSize(750, 900);
+    this.fr.setSize(300, 900);
 
 
     this.contentPane = new JPanel();
@@ -45,8 +53,10 @@ public class Quiz_E {
     this.menuItem2 = new JMenuItem("Info");
     this.mH.add(menuItem2);
 
-
-
+    this.label4= new JLabel("Das spiel wird ihr Leben verändern.");
+    this.contentPane.add(label4);
+    this.label5= new JLabel("Wollen Sie erfahren, Wie viel Zeit bleibt dir in deinem Leben noch?");
+    this.contentPane.add(label5);
     this.label1 = new JLabel("Wie ist Ihr Name?");
     this.contentPane.add(label1);
 
@@ -67,10 +77,27 @@ public class Quiz_E {
     this.button1 = new JButton("Erfahren");
     this.contentPane.add(button1);
 
+    this.j4 = new JTextField("Sie sterben in ___ Jahren!");
+    this.contentPane.add(j4);
+
+    this.label6 = new JLabel("Die Wahrheit ist schwer zu ertragen.");
+    this.contentPane.add(label6);
+
+    this.label7 = new JLabel("ich hab eine Lösung für dich!");
+    this.contentPane.add(label7);
 
     this.fr.setVisible(true);
 
+
+
+
+
   }
+
+
+
+
+
 
   public static void main(String[] args) {
     Quiz_E q = new Quiz_E();
