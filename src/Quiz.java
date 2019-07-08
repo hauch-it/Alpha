4974caf7884333dadee1fb7e7463fafbaa8b9fa7
Komlovski <evgeny.komlovsky@gmx.de>
2019-07-08 21:23:24 +0200

    Quiz Spiel. Änderungen

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 import javax.swing.JFrame;
@@ -21,8 +22,9 @@ public class Quiz extends JFrame {
     JLabel label2 = null;
     JLabel label3 = null;
     JLabel frage1 = null;
-    JLabel frage1antworten = null;
-
+    JLabel frage1antworten1 = null;
+  JLabel frage2 = null;
+  JLabel frage1antworten2 = null;
     JMenuItem menuItem1 = null;
     JMenuItem menuItem2 = null;
 
@@ -64,17 +66,23 @@ public class Quiz extends JFrame {
       this.label2 = new JLabel("Beantworten Sie uns 5 kurze Fragen und gewinnen Sie 10000 Euro!");
       this.contentPane.add(label2);
 
+
+
+
       this.frage1 = new JLabel("Frage 1 lautet: Was gehört nicht zum Test Driven Developement?");
+
       this.contentPane.add(frage1);
 
-      this.frage1antworten = new JLabel("A : Refactoring  B : Transformation   C : Clean Code  D : Neuer Test ");
-      this.contentPane.add(frage1antworten);
+      this.frage1antworten1 = new JLabel("A : Refactoring  B : Transformation   C : Clean Code  D : Neuer Test ");
+      this.contentPane.add(frage1antworten1);
 
       this.label3 = new JLabel("Antwort:");
       this.contentPane.add(label3);
       this.j1 = new JTextField("", 20);
       this.contentPane.add(j1);
       this.j1.setSize(250,30);;
+
+
 
 
 
@@ -106,6 +114,10 @@ public class Quiz extends JFrame {
         if(j1.getText().startsWith("C")  || j1.getText().startsWith("c") ) {
           System.out.println("ja");
           j1.setText("Richtig !  Gewinn 1000 Euro");
+          JOptionPane.showMessageDialog(null,
+              "Herzlichen Glückwunsch , Sie sind nicht so dumm, wie ich dachte !",
+              "Eine Nachricht",
+              JOptionPane.WARNING_MESSAGE);
 
           button1warsovielmalgedrückt++;
         }else{
