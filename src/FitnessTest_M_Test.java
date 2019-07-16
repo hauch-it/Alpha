50a965aa13c324f347ec8af67398409ee8fe3c37
MSeibert97 <michael.seibert@informatik.hs-fulda.de>
2019-07-16 13:24:15 +0200

    Der siebte Commit. Weitere Testfälle.

--- src/FitnessTest_M_Test.java
+++ src/FitnessTest_M_Test.java
@@ -26,4 +26,79 @@ public class FitnessTest_M_Test {
         //assert
         assertThat("", bmiResult, equalTo(expectedResult));
     }
+
+    @Test
+    public void mediumBmi() {
+        //arrange
+        FitnessTest_M fitnessTest_M = new FitnessTest_M();
+        float height = (float) 1.80;
+        float weight = 90;
+        int expectedResult = 27;
+
+        //act
+        int bmiResult = fitnessTest_M.calculateBmi(height, weight);
+
+        //assert
+        assertThat("", bmiResult, equalTo(expectedResult));
+    }
+
+    @Test
+    public void heavyBmi() {
+        //arrange
+        FitnessTest_M fitnessTest_M = new FitnessTest_M();
+        float height = (float) 1.60;
+        float weight = 100;
+        int expectedResult = 39;
+
+        //act
+        int bmiResult = fitnessTest_M.calculateBmi(height, weight);
+
+        //assert
+        assertThat("", bmiResult, equalTo(expectedResult));
+    }
+
+    @Test
+    public void lightBmi() {
+        //arrange
+        FitnessTest_M fitnessTest_M = new FitnessTest_M();
+        float height = (float) 1.75;
+        float weight = 50;
+        int expectedResult = 16;
+
+        //act
+        int bmiResult = fitnessTest_M.calculateBmi(height, weight);
+
+        //assert
+        assertThat("", bmiResult, equalTo(expectedResult));
+    }
+
+    @Test
+    public void monsterBmi() {
+        //arrange
+        FitnessTest_M fitnessTest_M = new FitnessTest_M();
+        float height = (float) 3.70;
+        float weight = 3000;
+        int expectedResult = 219;
+
+        //act
+        int bmiResult = fitnessTest_M.calculateBmi(height, weight);
+
+        //assert
+        assertThat("", bmiResult, equalTo(expectedResult));
+    }
+
+    @Test
+    public void antBmi() {
+        //arrange
+        FitnessTest_M fitnessTest_M = new FitnessTest_M();
+        float height = (float) 0.20;
+        float weight = (float) 0.30;
+        int expectedResult = 7;
+
+        //act
+        int bmiResult = fitnessTest_M.calculateBmi(height, weight);
+
+        //assert
+        assertThat("", bmiResult, equalTo(expectedResult));
+    }
 }
